47a623903b28bc914552e040b081b6e11a27bf2f
poswalsameer <poswalsameer2003@gmail.com>
2024-01-30 19:07:32 +0530

    setup of the project plust basic things

---
+++ typemaster/src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css'
+import Input from './components/Input'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <div className='h-full w-full bg-slate-600 flex flex-col justify-center items-center ' >
+
+        <p className='text-yellow-400 text-8xl font-extrabold' >
+          TYPE MASTER
+        </p>
+
+        <Input />
+
+      </div>
+
+
+    </>
+  )
+}
+
+export default App
---
+++ typemaster/src/components/Input.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Input() {
+  return (
+    <>
+      
+        <input type="text" id="inputBox" className='h-16 w-[40rem] p-5 bg-white rounded-xl text-black font-bold border-4 border-black outline-none focus:border-4 focus:border-blue-600' />
+
+    </>
+  )
+}
+
+export default Input
