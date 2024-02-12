c803d4f501b63407a99de19fd90b0800a3e2ea03
poswalsameer <poswalsameer2003@gmail.com>
2024-02-12 19:11:53 +0530

    added the feature of different colors on right and wrong words

--- typemaster/src/App.jsx
+++ typemaster/src/App.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react'
 import './App.css'
 import {generate, count} from 'random-words';
 
-const generatedWords = generate(33);
+let generatedWords = generate(45);
 
 function App() {
   
@@ -11,12 +11,35 @@ function App() {
   // const generatedWords = generate(33);
   const [currentWord, setCurrentWord] = useState('');
   const [activeWord, setActiveWord] = useState(0);
+  const [correctWord, setCorrectWord] = useState(Array(generatedWords.length).fill(false));
 
   const spaceClicked = (word) => {
 
     if( word.endsWith(' ')){
+
+      // word = word.slice(0, word.length-1);
+
+      if( word.trim()  === generatedWords[activeWord] ){
+        const newCorrectWords = [...correctWord];
+        newCorrectWords[activeWord] = true;
+        setCorrectWord(newCorrectWords);
+      }
+      else{
+        const newCorrectWords = [...correctWord];
+        newCorrectWords[activeWord] = false;
+        setCorrectWord(newCorrectWords);
+        
+      }
+
       setActiveWord( index => index+1 );
       setCurrentWord('');
+
+      // if( index > 33 ){
+      //   generatedWords = generate(33);
+      //   setCurrentWord('');
+      // }
+
+      
     }
 
     else{
@@ -35,10 +58,14 @@ function App() {
           TYPE MASTER
         </p>
 
-        <div className=' h-56 w-3/4 border-4 p-5 border-blue-300 rounded-2xl bg-black text-white flex flex-row flex-wrap'>
+        <div className=' h-80 w-3/4 border-4 p-5 border-blue-300 rounded-2xl bg-black text-white flex flex-row flex-wrap'>
 
+        {/* index === activeWord ? correctWord ? "text-green-300" : "text-red-400" : "text-white" */}
                 
-                {generatedWords.map( (eachWord, index) => ( <p className= {`m-3 text-2xl font-bold ${index === activeWord ? "text-yellow-300" : "text-white" } `} > {eachWord} </p> ) )}
+                {generatedWords.map( (eachWord, index) => ( <p className= {`m-3 text-2xl font-bold ${index < activeWord && correctWord[index]
+                ? 'text-green-500' : 'text-red-500' } ${ index > activeWord ? 'text-slate-200' : '' } 
+                
+                `} > {eachWord} </p> ) )}
 
         </div>
 
